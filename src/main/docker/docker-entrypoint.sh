c274de0e9298e1ab0dbfdcb05860b193ddae1fc2
tawanda <tawanda@turfsport.co.za>
2020-10-26 22:13:30 +0200

    renaming

--- src/main/docker/docker-entrypoint.sh
+++ src/main/docker/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-java $JAVA_OPTS -DDB_HOST=$DB_HOST -DDB_PORT=$DB_PORT -jar /dropwizard-webapp.jar db migrate application.yml
+java $JAVA_OPTS -DDB_HOST=$DB_HOST -DDB_PORT=$DB_PORT -jar /freshd-service.jar db migrate application.yml
 
 if [ ! $? -ne 0 ]; then
-  exec java $JAVA_OPTS -DDB_HOST=$DB_HOST -DDB_PORT=$DB_PORT -jar /dropwizard-webapp.jar server application.yml	
+  exec java $JAVA_OPTS -DDB_HOST=$DB_HOST -DDB_PORT=$DB_PORT -jar /freshd-service.jar server application.yml
 fi
 
 exec "$@"
