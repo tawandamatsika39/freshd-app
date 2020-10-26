f9b5ee518cc7fc33d42439e60adb05e8efe9f0e6
tawanda <tawanda@turfsport.co.za>
2020-10-26 21:38:25 +0200

    removed unnecessary files and folders

--- target/docker/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-java $JAVA_OPTS -DDB_HOST=$DB_HOST -DDB_PORT=$DB_PORT -jar /dropwizard-webapp.jar db migrate application.yml
-
-if [ ! $? -ne 0 ]; then
-  exec java $JAVA_OPTS -DDB_HOST=$DB_HOST -DDB_PORT=$DB_PORT -jar /dropwizard-webapp.jar server application.yml	
-fi
-
-exec "$@"
