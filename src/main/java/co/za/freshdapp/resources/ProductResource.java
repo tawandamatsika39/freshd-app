d2115662d357bcd2b23d29f374f1eaa157121ee6
tawandamatsika39 <3580059@ myuwc.ac.za>
2021-01-16 20:11:58 +0200

    fixing imports

--- src/main/java/co/za/freshdapp/resources/ProductResource.java
+++ src/main/java/co/za/freshdapp/resources/ProductResource.java
@@ -4,7 +4,13 @@ package co.za.freshdapp.resources;
 import co.za.freshdapp.dao.ProductDAO;
 import co.za.freshdapp.models.Product;
 import java.util.UUID;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.skife.jdbi.v2.DBI;
 
